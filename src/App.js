92d251f24c069fb61395a9e54d45667a10b9e816
YourUserNameOnGithubttaha361 <taha33oren@gmail.com>
2024-03-16 18:08:20 +0300

    howto start

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './components/Header';
 import About from './components/About';
 import Features from './components/Features';
+import HowTo from './components/HowTo';
 function App() {
   return (
     <div className="App">
       <Header/>
       <About/>
       <Features/>
+      <HowTo/>
     </div>
   );
 }
--- src/components/HowTo.js
+++ src/components/HowTo.js
@@ -1,10 +1,35 @@
+import "../styles/FeatureStyle.css"
+import howto1 from "../materials/howto.jpeg"
+import howto2 from "../materials/howto2.jpeg"
+import howto3 from "../materials/howto3.png"
+import howto4 from "../materials/howto4.jpeg"
 function HowTo() {
-    return (
-      <div>
-          <h1>HowTo</h1>
+  return (
+    <div className="MainFeaturesClass">
+      <div className="TextImageContainer">
+        <div className="FeatureClass">
+          <div className="FeatureTextLeft">
+            <h4>SHOWCASE THE FEATURES</h4>
+            <h1>Show people the functionalities of your product</h1>
+            <p>With simple yet modern design, users will get to know what are you offering them</p>
+          </div>
+          <div className="FeatureImageRight">
+            <img src={howto1} alt="" />
+          </div>
+        </div>
+        <div className="FeatureClass">
+          <div className="FeatureImageLeft">
+            <img id="herbalimage" src={howto2} alt="" />
+          </div>
+          <div className="FeatureTextRight">
+            <h4>SHOWCASE THE FEATURES</h4>
+            <h1>Show people the functionalities of your product</h1>
+            <p>With simple yet modern design, users will get to know what are you offering them</p>
+          </div>
+        </div>
       </div>
-    );
-  }
-  
-  export default HowTo;
-  
+    </div>
+  );
+}
+
+export default HowTo;
